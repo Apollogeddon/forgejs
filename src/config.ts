158bcb1e489d24c134dc83e0c833f29921c3ae92
Apollogeddon <dunc1221@proton.me>
2025-12-15 21:04:17 +1030

    ci(config): align build config paths and tool names

--- src/config.ts
+++ src/config.ts
@@ -50,6 +50,7 @@ export default Configuration;
 export const tsupConfig = `\
 import baseConfig from "@apollogeddon/forgejs/tsup.config.cjs";
 import { defineConfig } from "tsup";
+
 export default defineConfig({
   ...baseConfig,
 });
